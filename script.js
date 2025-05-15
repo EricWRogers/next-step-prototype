b15608fe3e0b9bc90f1c21db0a81bd135b46f70b
EricWRogers <ericwilliamrogers@gmail.com>
2025-05-14 22:44:16 -0500

    dashboard mochup

---
+++ script.js
@@ -0,0 +1,8 @@
+lucide.createIcons();
+
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelectorAll('.nav-item').forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+      });
+});
